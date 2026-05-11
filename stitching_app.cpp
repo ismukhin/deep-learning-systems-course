f79bd58d0cbb267dd19d1e117f5aa102749cd2f9
ismukhin <vvstanka39@gmail.com>
2026-05-11 23:46:33 +0300

    update

--- stitching_app.cpp
+++ stitching_app.cpp
@@ -184,16 +184,16 @@ int main(int argc, char* argv[]) {
     imwrite(result_name, pano);
     std::cout << "stitching completed successfully\n" << result_name << " saved!" << std::endl;
 
-    cv::Mat img = cv::imread(cv::samples::findFile("../result_ground.jpg"));
+    cv::Mat img = cv::imread(cv::samples::findFile("../result.jpg"));
     std::cout << img.size() << std::endl;
     std::cout << pano.size() << std::endl;
-    //if (cv::countNonZero(img != pano) == 0) {
-    //    std::cout << "Test succeed" << std::endl;
-    //    return EXIT_SUCCESS;
-    //} else {
-    //    std::cout << "Images not equal" << std::endl;
-    //    return EXIT_FAILURE;
-    //}
+    if (cv::countNonZero(img != pano) == 0) {
+        std::cout << "Test succeed" << std::endl;
+        return EXIT_SUCCESS;
+    } else {
+        std::cout << "Images not equal" << std::endl;
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
